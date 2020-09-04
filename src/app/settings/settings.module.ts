6d3bc18c3ffb3baf9f42e8997afdb313288f58a5
asunder123 <50335583+asunder123@users.noreply.github.com>
2020-09-03 23:37:49 -0400

    Update settings.module.ts
    Changes to settings.module.ts

--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -1,8 +1,4 @@
-import { ModuleWithProviders, NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-
 import { SettingsComponent } from './settings.component';
-import { AuthGuard } from '../core';
 import { SharedModule } from '../shared';
 import { SettingsRoutingModule } from './settings-routing.module';
 

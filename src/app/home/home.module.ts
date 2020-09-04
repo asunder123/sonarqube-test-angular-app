00af0324aedf55ee7009e547fdbb6096147878f4
asunder123 <50335583+asunder123@users.noreply.github.com>
2020-09-03 23:48:18 -0400

    Update home.module.ts
    Debt reduction or changes

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,6 +1,3 @@
-import { ModuleWithProviders, NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-
 import { HomeComponent } from './home.component';
 import { HomeAuthResolver } from './home-auth-resolver.service';
 import { SharedModule } from '../shared';
